19ca9a3e7830ab7069ff87be504915739fb12eaf
Professional DIgi <dewilliams01@gmail.com>
2025-08-14 07:45:36 -0400

    Update seo_analyzer.py

--- backend/services/seo_analyzer.py
+++ backend/services/seo_analyzer.py
@@ -1,4 +1,4 @@
-from seoanalyzer import analyze
+from seo_analyzer import analyze
 
 def run_seo_analysis(url):
     results = analyze(url, analyze_headings=True, analyze_keywords=True, analyze_description=True)
